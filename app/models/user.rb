6aecc545b893a15237c68e4ecbe3596ca197bac8
jonahthompson <jonahthompson12@gmail.com>
2017-11-29 16:02:10 -0700

    department partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 
-	 has_many :project, through: :user_projects
-   has_many :tasks, through: :user_tasks
-   belongs_to :department
+  has_many :project, through: :user_projects
+  has_many :tasks, through: :user_tasks
+  belongs_to :department, optional: true
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
