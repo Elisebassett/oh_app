9961feccf58738fc1a476657fd4ebb94b38eeef2
jonahthompson <jonahthompson12@gmail.com>
2017-11-30 13:51:15 -0700

    task modal and create working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,22 @@
 class TasksController < ApplicationController
 
   def create
-  
+  	@task = Task.new(task_params)
+  		if @task.save!
+  		end
   end
 
   def update
-  
+  	
   end
 
   def destroy
   
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:project_id, :name, :description, :deadline)
   end
 end
