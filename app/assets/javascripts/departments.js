44d687b8b2dcc84155b1a0910a8552554ff7c909
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-01 14:25:15 -0700

    delete buttons work

--- app/assets/javascripts/departments.js
+++ app/assets/javascripts/departments.js
@@ -2,13 +2,10 @@ $(document).on('turbolinks:load', function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 	$('.modal').modal();
 	// $('modal1').modal('open');
-});
-
-	$(document).on('turbolinks:load', function(){
-		$('.destroy_d').on('ajax:success', function(){
-		$('.' + window.department_section).remove()
-			$('.destroy_d').on('click', function(){
-				window.department_section = $(this).closest('.department').attr('class').split(' ')[4];
-			});  	
-		});
+	$('.destroy_d').on('ajax:success', function(){
+		$('.' + window.department_section).remove(); 	
 	});
+	$('.destroy_d').on('click', function(){
+		window.department_section = $(this).closest('.department').attr('class').split(' ')[4];
+	}); 
+});
--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,7 +1,11 @@
 $(document).on ('turbolinks:load', function (){
 	$('.destroy_task').on('ajax:success', function(){
 		$(this).closest('.tasks').remove()
+			$('.' + window.task_section).remove(); 
 	});
+	$('.destroy_task').on('click', function(){
+		window.task_section = $(this).closest('.tasks').attr('class').split(' ')[1];
+	}); 
 });
 
 		
