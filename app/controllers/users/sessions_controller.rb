257d93a489999c7c55b1570a982ddd8e88c28179
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-21 13:19:55 -0700

    user controller update

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,6 +16,6 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
